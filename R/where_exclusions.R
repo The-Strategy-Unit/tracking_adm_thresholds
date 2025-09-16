cca3a9222ffffce6c5e1dbe5933763f227b7a770
Andrew Jones <andrew.jones40@udal.nhs.uk>
2025-09-16 11:32:33 +0100

    Update where exclusions R script to reflect sql

--- R/where_exclusions.R
+++ R/where_exclusions.R
@@ -15,38 +15,35 @@ gc()
 gc()
 
 df_where_exclusions <- df_where_exclusions_raw |> 
-  rename(
-    procode = der_provider_code,
-    # `streamed/left/died` = disstat_incomplete
-    ) |> 
+  rename(procode = der_provider_code) |> 
   arrange(procode, desc(n)) |> 
   group_by(procode) |> 
   mutate(p = n/sum(n)) |> 
   ungroup() 
 
-df_where_exclusions |> 
-  filter(procode == "RC9") |> 
-  # print(n=40)
-  identity()
+# df_where_exclusions |> 
+#   filter(procode == "RC9") |> 
+#   # print(n=40)
+#   identity()
 
 df_where_exclusions |> saveRDS(here("data", "where_exclusion_matrix.rds"))
-df_where_exclusions <- readRDS(here("data", "where_exclusion_matrix.rds"))
+# df_where_exclusions <- readRDS(here("data", "where_exclusion_matrix.rds"))
 
-df_where_exclusions |> 
-  filter(procode == "RC9") |> 
-  mutate(sigma = sum(n)) |>
-  rowwise() |> 
-  mutate(flag_sum = sum(c_across(c(everything(), -c(procode, n, p, sigma))))) |> 
-  ungroup() |> 
-  # REMOVE THE ROW CORRESPONDING TO ATTENDANCES FOLLOWING ALL EXCLUSIONS:
-  filter(flag_sum != 0) |> 
-  mutate(n_after_excl = sigma - cumsum(n)) |> 
-  mutate(n_before_excl = n_after_excl + n) |> 
-  select(-c(sigma, p)) |> 
-  rename(n_excl = n) |> 
-  relocate(c(n_before_excl, n_excl, n_after_excl), .after = flag_sum) |> 
-  # print(n=40)
-  view("where")
+# df_where_exclusions |> 
+#   filter(procode == "RC9") |> 
+#   mutate(sigma = sum(n)) |>
+#   rowwise() |> 
+#   mutate(flag_sum = sum(c_across(c(everything(), -c(procode, n, p, sigma))))) |> 
+#   ungroup() |> 
+#   # REMOVE THE ROW CORRESPONDING TO ATTENDANCES FOLLOWING ALL EXCLUSIONS:
+#   filter(flag_sum != 0) |> 
+#   mutate(n_after_excl = sigma - cumsum(n)) |> 
+#   mutate(n_before_excl = n_after_excl + n) |> 
+#   select(-c(sigma, p)) |> 
+#   rename(n_excl = n) |> 
+#   relocate(c(n_before_excl, n_excl, n_after_excl), .after = flag_sum) |> 
+#   # print(n=40)
+#   view("where")
 
 # ALL PROVIDERS:
 
