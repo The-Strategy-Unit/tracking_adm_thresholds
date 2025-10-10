b6a57c3ce70db646e6b996a1572c587297c9bdd2
Andrew Jones <andrew.jones40@udal.nhs.uk>
2025-10-10 13:45:59 +0100

    Add R script to load lib and set up connections

---
+++ R/01_setup.R
@@ -0,0 +1,40 @@
+# README
+# Load packages and establish DB connection. 
+# Note: you will be prompted for a password via pop-up.
+
+library("DBI")
+# library("pak")
+library("here")
+library("odbc")
+library("mgcv")
+library("broom")
+library("dplyr")
+library("purrr")
+library("furrr")
+library("tidyr")
+library("tibble")
+library("forcats")
+library("ggplot2")
+library("ggrepel")
+library("janitor")
+library("keyring")
+library("stringr")
+library("tsibble")
+library("lubridate")
+library("patchwork")
+library("yardstick")
+
+options(scipen=999) 
+
+server <- keyring::key_get("server")
+db <- keyring::key_get("db")
+
+con_test <- DBI::dbConnect(
+  odbc::odbc(),
+  Driver = "ODBC Driver 17 for SQL Server",
+  Server = server,
+  Database = db,
+  Authentication = "ActiveDirectoryInteractive"
+)
+
+# REMEMBER POP-UP!
