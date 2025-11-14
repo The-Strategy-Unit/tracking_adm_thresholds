9fd32a24b44165e2c88ae97d720e9fb4dc606f30
Andrew Jones <andrew.jones40@udal.nhs.uk>
2025-11-14 15:36:20 +0100

    Update R model script

--- R/04_model.R
+++ R/04_model.R
@@ -2,43 +2,90 @@
 # Fit models for each provider site, in parallel. 
 # Calculate model performance metrics and capture model results.
 
+# rm(df_ecds_joined)
+# rm(df_var_engineering_1of2)
+# rm(df_providers_nested)
+# rm(df_providers_nested_specs)
+rm(df_ecds_raw)
+
 gc()
 gc()
 gc()
 
-# -------------------------------------------------------------------------
+df_prov_low_admirate <- df_var_engineering_2of2 |>
+  mutate(p_adm = map_dbl(data, function(df) {
+    df |>
+      count(admitted) |>
+      mutate(p = n/sum(n)) |>
+      filter(admitted == 1) |> 
+      pull(p)
+  }
+  )) |>
+  # mutate(p_adm = abs(p)) |>
+  # mutate(site_name = map_chr(data, \(df) df |> distinct(site_name) |> pull() |> as.character())) |>
+  distinct(der_provider_site_code, ed_name, p_adm) |>
+  arrange(p_adm) |>
+  filter(p_adm <0.05)
+  # print(n= 20)
+  # view("p_adm")
+  # identity()
+
+
+
+# SAMPLE -------------------------------------------------------------------------
+
+# df_model_test <- df_var_engineering_2of2 |> 
+#   inner_join(df_mixed, join_by(der_provider_site_code == value)) |>
+#   mutate(ed_name = map_chr(data, \(df) df |> distinct(site_name) |> pull() |> as.character())) |> 
+#   # count(dq, der_provider_site_code, ed_name) |> 
+#   identity()
+
+
+# END SAMPLE ----------------------------------------------------------------
 
 plan(multisession, workers = 4)
 future::futureSessionInfo()
 
 # TODO IS FUTURE MAP2 VIABLE (TOO SLOW VS FUTURE MAP?)
 tictoc::tic()
-df_models <- df_var_engineering_2of2 |> 
+# df_models <- df_var_engineering_2of2 |>
+set.seed(1001)
+df_models <-
+  df_var_engineering_2of2 |>
   ### OPTION FOR DEMO:
+  anti_join(df_prov_low_admirate, join_by(der_provider_site_code)) |> 
+  filter(
+    der_provider_site_code %in% sample(vec_providers_diag, 2) |
+      der_provider_site_code %in% vec_providers_diag_inconstant |
+      der_provider_site_code %in% sample(vec_providers_complaint_candidates, 2) |
+      str_detect(ed_name, "Hull") |
+      str_detect(ed_name, "St George")
+  ) |> 
   ### TAKE ONE FROM BETTER GROUP, ONE FROM WORSE:
   # slice(c(1:2, 357:358)) |> 
-  filter(der_provider_site_code %in% c(
-    "RWA01", # Hull Royal Infirmary
-    "RQWG0"  # The Princess Alexandra Hospital
-    )
-    )|>
+  # filter(der_provider_site_code %in% c(
+  #   "RWA01", # Hull Royal Infirmary
+  #   "RQWG0"  # The Princess Alexandra Hospital
+  #   )
+  #   )|>
   ###
   ###
   mutate(mod = future_map2(data, mod_formula, function(df, x) {
     mgcv::gam(
       formula = as.formula(x),
-      # formula = ed_discharged ~ year_week + s(age, by = sex) + sex + imd_quint + ethnic_grp_sus + wkend + night_time_8to8 + diag01 + arrmode_ambulance + acuity + referral_source,
+      # formula = admitted ~ year_week + s(age, by = sex) + sex + imd_quint + ethnic_grp_sus + wkend + night_time_8to8 + diag01 + arrmode_ambulance + acuity + referral_source,
       family = "binomial",
       data = df
     ) 
   }))
 plan(sequential)
+gc()
+gc()
+gc()
 tictoc::toc()
-gc()
-gc()
-gc()
 
-# APPROX 375.25 secs for 4 models, 4 workers 
+# APPROX 375 secs for 4 models, 4 workers 
+# APPROX 800 secs for 16 models, 4 workers 
 
 # 7. METRICS AND RELATIONSHIP TO DQ --------------------------------
 
@@ -46,7 +93,7 @@ df_models |>
   # TODO NOT SURE BRIER SCORE IS CORRECT:
   mutate(brier = map2(data, mod, function(df, x) {
     tibble(
-      obs = df$ed_discharged,
+      obs = df$admitted,
       # fit = 1-fitted(x), # predicted probabilities 1
       fit = fitted(x), # predicted probabilities 1
     ) |>
@@ -54,7 +101,7 @@ df_models |>
     })) |>
   mutate(roc = map2(data, mod, function(df, x) {
     tibble(
-      obs = df$ed_discharged,
+      obs = df$admitted,
       fit = fitted(x), # predicted probabilities 1
     ) |>
       roc_auc(obs, fit, event_level = "second") 
@@ -73,12 +120,13 @@ df_models |>
     df_provider_dataqual_mean |>
       select(1,2,4),
     join_by(der_provider_site_code)
-    )
+    ) |> 
+  filter(mod_spec %in% c("a", "b"))
 
 
 # TABLE OF COEFFICIENTS -----------------------------------------------------------------
 
-df_models$mod[[3]] 
+# df_models$mod[[3]] 
   
 df_coeff <- df_models |> 
   mutate(mod_coeffs = map(mod, function(m) {
@@ -99,4 +147,4 @@ df_coeff <- df_models |>
 
   }))
 
-df_coeff$mod_coeffs[[1]]
+# df_coeff$mod_coeffs[[1]]
