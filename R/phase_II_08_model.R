aba52e97e02ebe51407108003069abf9a3b72f9a
Andrew Jones <andrew.jones40@udal.nhs.uk>
2025-12-12 14:32:38 +0100

    Update basic phase II model script

--- R/phase_II_08_model.R
+++ R/phase_II_08_model.R
@@ -1,5 +1,6 @@
 # README
 # Fit the model to a data sample of X million.
+# Cab be run as background job.
 
 library("here")
 library("mgcv")
@@ -18,44 +19,25 @@ library("yardstick")
 
 options(scipen=999) 
 
-# 1m rows = 26 mins
+# 1m rows ~ 26 mins
 
 
 # df_var_ref_levels |>
 #     arrow::write_parquet(
-#       sink = here("data", "251203_df_var_ref_levels.gzip.parquet"),
+#       sink = here("data", "251208_df_var_ref_levels.gzip.parquet"),
 #       compression = "gzip"
 #     )
 
-df_var_ref_levels <- arrow::open_dataset(here::here("data", "251203_df_var_ref_levels.gzip.parquet")) |>
+df_var_ref_levels <- arrow::open_dataset(here::here("data", "251208_df_var_ref_levels.gzip.parquet")) |>
   collect()
-
-# TODO THIS - OR WHATEVER THE SOLUTION IS - SHOULD GO IN DATA PREP:
-df_var_ref_levels <- df_var_ref_levels |> 
-  mutate(acuity = as.character(acuity)) |> 
-  mutate(acuity = case_when(
-    site_name == "Great Western Hospital A&E" &
-      acuity %in% c("Standard", "Urgent", "Non-urgent") ~ "NonUrg/Std/Urg",
-    site_name == "Worthing Hospital" &
-      acuity %in% c("Standard", "Non-urgent") ~ "NonUrg/Std",
-    site_name == "Leicester Royal Infirmary" &
-      acuity %in% c("Standard", "Urgent") ~ "Std/Urg",
-    T ~ acuity
-  )) |> 
-  mutate(acuity = fct_relevel(acuity, "Standard"))
-  
-
-# df_var_ref_levels %>%
-#   # colnames(df_ecds_II_raw)[c()] %>%
-#   colnames() %>%
-#   map(~ count(df_var_ref_levels, is.na(.data[[.x]]), sort = T))
 
 set.seed(1001)
 df_var_ref_levels_sample_1m <- df_var_ref_levels |>
   slice_sample(n = 1e6)
 
 
-mod_run_2 <-
+# _4. ACUITY MERGED, COVID ADDED, NEW REF LEVEL FOR QUARTER
+mod_run_6 <-
   mgcv::gam(
     formula = admitted ~ year_quarter +
       s(age, by = sex) + sex + imd_quint + ethnic_grp_sus +
@@ -68,4 +50,4 @@ mod_run_2 <-
     data = df_var_ref_levels_sample_1m
   )
 
-mod_run_2 |> saveRDS(here::here("data", "251205_mod_run_2_acuity.rds"))
+mod_run_6 |> saveRDS(here::here("data", "251208_mod_run_6_covid_v2.rds"))
